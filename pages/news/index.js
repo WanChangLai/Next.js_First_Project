32fa790398d42ab5e6235178e94ef952b48fe354
Wan Chang Lai <wanchang.lai@live.com>
2022-12-01 20:45:40 +0800

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,22 @@
 // our-domain.com/news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a great-framework">
+            Next JS is a Great IDE.
+          </Link>
+        </li>
+        <li>Ful stack Framework.</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
