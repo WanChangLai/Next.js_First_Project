3ca0662c8d9190f6f569b9ff778c6e803470b454
Wan Chang Lai <wanchang.lai@live.com>
2022-12-01 20:23:04 +0800

    Retrive The Parameter Data

--- pages/news/[newId].js
+++ pages/news/[newId].js
@@ -1,8 +1,17 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/anything
 //special syntax [] means anything
 
 function DetailPage() {
-  return <h1>The Detail Page</h1>;
+  const router = useRouter();
+
+  const newsId = router.query.newId;
+
+  //send to request to API
+  //to fetch the news item with newsId
+
+  return (<h1>The Detail Page</h1>), (<p>{newsId}</p>);
 }
 
 export default DetailPage;
